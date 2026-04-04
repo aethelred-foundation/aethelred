dbacc40cad736d184bd9831c456fd7913397a0b8
github-action-benchmark <github@users.noreply.github.com>
2026-04-04 22:56:45 +0000

    add Benchmark (cargo) benchmark result for 29791f1fcc8db41d06d8ca0ba4ec43a5858d6e86

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1775343405629,
+  "repoUrl": "https://github.com/aethelred-foundation/aethelred",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rameshtamilselvan@gmail.com",
+            "name": "Ramesh Tamilselvan",
+            "username": "ramtamilselvan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29791f1fcc8db41d06d8ca0ba4ec43a5858d6e86",
+          "message": "Fix workflow gating and benchmark write permissions (#56)",
+          "timestamp": "2026-04-05T02:55:25+04:00",
+          "tree_id": "9e5a6d4b62ec762ca7c78b50c1334bab214ab400",
+          "url": "https://github.com/aethelred-foundation/aethelred/commit/29791f1fcc8db41d06d8ca0ba4ec43a5858d6e86"
+        },
+        "date": 1775343404447,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "sha256_4kb",
+            "value": 2652,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
