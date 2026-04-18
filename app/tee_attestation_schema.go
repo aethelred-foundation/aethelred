a2dd71cc087a504a816c8e0fa97f3dba36a96f7b
Ramesh Tamilselvan <rameshtamilselvan@gmail.com>
2026-04-18 20:45:34 +0400

    app: restore nitro quote schema contract

--- app/tee_attestation_schema.go
+++ app/tee_attestation_schema.go
@@ -26,15 +26,16 @@ func validateTEEQuoteSchema(att *TEEAttestationData) error {
 }
 
 type nitroQuoteSchema struct {
-	ModuleID    string          `json:"module_id"`
-	Timestamp   int64           `json:"timestamp_unix"`
-	Digest      string          `json:"digest"`
-	PCRs        []nitroQuotePCR `json:"pcrs"`
-	Certificate []byte          `json:"certificate,omitempty"`
-	CABundle    [][]byte        `json:"cabundle,omitempty"`
-	PublicKey   []byte          `json:"public_key,omitempty"`
-	UserData    []byte          `json:"user_data,omitempty"`
-	Nonce       []byte          `json:"nonce,omitempty"`
+	ModuleID            string          `json:"module_id"`
+	Timestamp           int64           `json:"timestamp_unix"`
+	Digest              string          `json:"digest"`
+	PCRs                []nitroQuotePCR `json:"pcrs"`
+	Certificate         []byte          `json:"certificate,omitempty"`
+	CABundle            []byte          `json:"cabundle,omitempty"`
+	PublicKey           []byte          `json:"public_key,omitempty"`
+	UserData            []byte          `json:"user_data,omitempty"`
+	Nonce               []byte          `json:"nonce,omitempty"`
+	SimulationSignature []byte          `json:"simulation_signature,omitempty"`
 }
 
 func validateNitroQuoteSchema(platform string, quote []byte, userData []byte, nonce []byte) error {
